8051d4a28aa13a4fb36208428a81f1c75180ac17
lamphan <ponpy30@gmail.com>
2022-10-01 18:11:13 +0700

    update controller

---
+++ WebApp/Areas/Admin/Controllers/ServicePacksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    public class ServicePacksController : Controller
+    {
+        private BikeStoreDbContext db = new BikeStoreDbContext();
+
+        // GET: Admin/ServicePacks
+        public ActionResult Index()
+        {
+            return View(db.service_packs.ToList());
+        }
+
+        // GET: Admin/ServicePacks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service_packs service_packs = db.service_packs.Find(id);
+            if (service_packs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service_packs);
+        }
+
+        // GET: Admin/ServicePacks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ServicePacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "service_pack_id,service_pack_name,price")] service_packs service_packs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.service_packs.Add(service_packs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(service_packs);
+        }
+
+        // GET: Admin/ServicePacks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service_packs service_packs = db.service_packs.Find(id);
+            if (service_packs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service_packs);
+        }
+
+        // POST: Admin/ServicePacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "service_pack_id,service_pack_name,price")] service_packs service_packs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service_packs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service_packs);
+        }
+
+        // GET: Admin/ServicePacks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            service_packs service_packs = db.service_packs.Find(id);
+            if (service_packs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service_packs);
+        }
+
+        // POST: Admin/ServicePacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            service_packs service_packs = db.service_packs.Find(id);
+            db.service_packs.Remove(service_packs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApp/Areas/Admin/StaffsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin
+{
+    public class StaffsController : Controller
+    {
+        private BikeStoreDbContext db = new BikeStoreDbContext();
+
+        // GET: Admin/Staffs
+        public ActionResult Index()
+        {
+            var staffs = db.staffs.Include(s => s.user_logins);
+            return View(staffs.ToList());
+        }
+
+        // GET: Admin/Staffs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Admin/Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.staff_id = new SelectList(db.user_logins, "user_id", "user_name");
+            return View();
+        }
+
+        // POST: Admin/Staffs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "staff_id,first_name,last_name,phone,email,role")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.staff_id = new SelectList(db.user_logins, "user_id", "user_name", staff.staff_id);
+            return View(staff);
+        }
+
+        // GET: Admin/Staffs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.staff_id = new SelectList(db.user_logins, "user_id", "user_name", staff.staff_id);
+            return View(staff);
+        }
+
+        // POST: Admin/Staffs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "staff_id,first_name,last_name,phone,email,role")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.staff_id = new SelectList(db.user_logins, "user_id", "user_name", staff.staff_id);
+            return View(staff);
+        }
+
+        // GET: Admin/Staffs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Admin/Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            staff staff = db.staffs.Find(id);
+            db.staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
