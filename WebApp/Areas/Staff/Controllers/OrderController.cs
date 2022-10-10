92ad8799786b66112771561b0b0a28f201bbf017
mytran324 <92320923+mytran324@users.noreply.github.com>
2022-10-10 14:48:39 +0700

    order_saler

---
+++ WebApp/Areas/Staff/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Areas.Staff.Controllers
+{
+    public class OrderController : Controller
+    {
+        private BikeStoreDbContext db = new BikeStoreDbContext();
+        // GET: Staff/Order
+        public ActionResult Index()
+        {
+            List<order> Orders = db.orders.Where(s => s.order_status == 4).ToList();
+
+            return View(Orders);
+        }
+    }
+}
--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -19,5 +19,6 @@ namespace WebApp.Controllers
         {
             return View();
         }
+
     }
 }
