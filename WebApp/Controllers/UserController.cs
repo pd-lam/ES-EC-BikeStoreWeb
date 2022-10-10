c8136a62db869000836f840a4682189ff7eae28b
mytran324 <92320923+mytran324@users.noreply.github.com>
2022-10-10 17:51:02 +0700

    RU Order_saler

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class UserController : Controller
     {
+        private BikeStoreDbContext db = new BikeStoreDbContext();
         // GET: Login
         public ActionResult Login()
         {
@@ -19,6 +21,16 @@ namespace WebApp.Controllers
         {
             return View();
         }
+        public ActionResult Orders(string id)
+        {
+            List<order> orders = db.orders.Where(s => s.customer_id == id).ToList();
+            return View(orders);
+        }
+        public ActionResult DetailOrder(int? id)
+        {
+            List<order_items> Order_items = db.order_items.Where(s => s.order_id == id).ToList();
+            return View(Order_items);
 
+        }
     }
 }
